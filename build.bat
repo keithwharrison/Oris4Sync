f555dc134d7bd28257df7856e8ee371dc45084f8
Keith Harrison <keithharrison@oris4.com>
2014-03-18 12:11:39 -0300

    adding clean step to build script

--- build.bat
+++ build.bat
@@ -1,5 +1,13 @@
 call "%VS110COMNTOOLS%"vsvars32.bat
 
+devenv "CmisSync\Windows\CmisSync.sln" /Clean "Debug|x86" /Project "Installer"
+devenv "CmisSync\Windows\CmisSync.sln" /Clean "Debug|x64" /Project "Installer"
+devenv "CmisSync\Windows\CmisSync.sln" /Clean "Debug|x86" /Project "InstallerBootstrapper"
+
+devenv "CmisSync\Windows\CmisSync.sln" /Clean "Release|x86" /Project "Installer"
+devenv "CmisSync\Windows\CmisSync.sln" /Clean "Release|x64" /Project "Installer"
+devenv "CmisSync\Windows\CmisSync.sln" /Clean "Release|x86" /Project "InstallerBootstrapper"
+
 devenv "CmisSync\Windows\CmisSync.sln" /Build "Debug|x86" /Project "Installer"
 devenv "CmisSync\Windows\CmisSync.sln" /Build "Debug|x64" /Project "Installer"
 devenv "CmisSync\Windows\CmisSync.sln" /Build "Debug|x86" /Project "InstallerBootstrapper"
